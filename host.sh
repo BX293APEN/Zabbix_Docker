0d4d804315024df77766ba179a074d0c6bc024c2
PEN <uemasaki20011222@gmail.com>
2026-04-09 14:43:48 +0900

    zabbix

--- host.sh
+++
@@ -1,5 +0,0 @@
-docker exec -i Zabbix_DB mysql -uzabbix -pzabbixpass zabbix < ./zabbix-init/schema.sql
-docker exec -i Zabbix_DB mysql -uzabbix -pzabbixpass zabbix < ./zabbix-init/data.sql
-
-# User : Admin 
-# Password : zabbix
